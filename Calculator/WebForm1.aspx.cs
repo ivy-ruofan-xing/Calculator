0c29d72ffce937370524284f5ed9a330218618f6
Ruofan(Ivy) Xing <r5xing@uwaterloo.ca>
2017-12-31 03:16:12 +0800

    Completed simple calculator. Revolved previous refreshing issue with Session()

--- Calculator/WebForm1.aspx.cs
+++ Calculator/WebForm1.aspx.cs
@@ -13,21 +13,34 @@ namespace Calculator
         private string Expression { get; set; }
         private char LastEntry { get; set; }
         private Operation Operation = new Operation(0, "+");
-        private bool Started = false;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Started)
+            CurrNumberString = "0";
+            Expression = "";
+            LastEntry = '0';
+
+            if (IsPostBack)
             {
-                CurrNumberString = "0";
-                Expression = "";
-                LastEntry = '0';
-
-                ExpressionText.Text = Expression;
-                CurrNumberText.Text = CurrNumberString;
-
-                Started = true;
+                CurrNumberString = Session["CurrNumberString"].ToString();
+                Expression = Session["Expression"].ToString();
+                LastEntry = (char)Session["LastEntry"];
+                Operation = (Operation)Session["Operation"];
             }
+            else
+            {
+                Session["CurrNumberString"] = CurrNumberString;
+                Session["Expression"] = Expression;
+                Session["LastEntry"] = LastEntry;
+                Session["Operation"] = Operation;
+            }
+
+            if (!char.IsDigit(LastEntry))
+            {
+                ExpressionText.Text = Expression;
+            }
+            
+            CurrNumberText.Text = CurrNumberString;
             
         }
 
@@ -45,7 +58,16 @@ namespace Calculator
                 Operation.FirstNumber = Double.Parse(CurrNumberString);
                 Operation.Operand = ButtonText;
 
+                if (Expression == "")
+                {
+                    Expression = CurrNumberString;
+                }
                 Expression = Expression.Insert(Expression.Length, ButtonText);
+                LastEntry = Expression[Expression.Length - 1];
+            }
+            else if (LastEntry == '=')
+            {
+                Expression = CurrNumberString.Insert(CurrNumberString.Length, ButtonText);
                 LastEntry = Expression[Expression.Length - 1];
             }
             // if last entry is not a digit, aka it's an operand, update the operation and change the expression and last entry
@@ -53,13 +75,44 @@ namespace Calculator
             {
                 Operation.Operand = ButtonText;
 
-                Expression.Remove(Expression.Length);
+                if (Expression != "")
+                {
+                    Expression = Expression.Remove(Expression.Length - 1);
+                }
+                else
+                {
+                    Expression = CurrNumberString;
+                }
                 Expression = Expression.Insert(Expression.Length, ButtonText);
                 LastEntry = Expression[Expression.Length - 1];
             }
 
             ExpressionText.Text = Expression;
             CurrNumberText.Text = CurrNumberString;
+
+            if (Operation.OperationFlag == 1)
+            {
+                CurrNumberText.Text = "Cannot divide by zero";
+                CurrNumberString = "0";
+                Expression = "";
+                ExpressionText.Text = Expression;
+                LastEntry = '0';
+                Operation.OperationFlag = 0;
+            }
+            else if (Operation.OperationFlag == 2)
+            {
+                CurrNumberText.Text = "Result is undefined";
+                CurrNumberString = "0";
+                Expression = "";
+                ExpressionText.Text = Expression;
+                LastEntry = '0';
+                Operation.OperationFlag = 0;
+            }
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
+            Session["LastEntry"] = LastEntry;
+            Session["Operation"] = Operation;
         }
 
         protected void CEButton_Click(object sender, EventArgs e)
@@ -72,6 +125,9 @@ namespace Calculator
             CurrNumberString = "0";
 
             CurrNumberText.Text = CurrNumberString;
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
         }
 
         protected void CButton_Click(object sender, EventArgs e)
@@ -85,36 +141,72 @@ namespace Calculator
 
             Operation.FirstNumber = 0;
             Operation.Operand = "+";
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
+            Session["LastEntry"] = LastEntry;
+            Session["Operation"] = Operation;
         }
 
         protected void ButtonDEL_Click(object sender, EventArgs e)
         {
-            if (CurrNumberString.Length > 1)
+            if (char.IsDigit(LastEntry))
             {
-                CurrNumberString = CurrNumberString.Substring(0, CurrNumberString.Length - 1);
-                Expression = Expression.Substring(0, Expression.Length - 1);
-            }
-            else
-            {
-                if (CurrNumberString.Length == 1 & CurrNumberString != "0" )
+                if (CurrNumberString.Length > 1)
                 {
+                    CurrNumberString = CurrNumberString.Substring(0, CurrNumberString.Length - 1);
                     Expression = Expression.Substring(0, Expression.Length - 1);
                 }
-                CurrNumberString = "0";
+                else
+                {
+                    if (CurrNumberString.Length == 1 & CurrNumberString != "0")
+                    {
+                        Expression = Expression.Substring(0, Expression.Length - 1);
+                    }
+                    CurrNumberString = "0";
+                }
             }
 
             CurrNumberText.Text = CurrNumberString;
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
         }
 
         protected void ButtonEQU_Click(object sender, EventArgs e)
         {
             Operation.SecondNumber = Double.Parse(CurrNumberString);
             CurrNumberString = Math.Round(Operation.Calculate(), 10).ToString();
+            Operation.FirstNumber = Double.Parse(CurrNumberString);
             Expression = "";
-            LastEntry = '0';
+            LastEntry = '=';
 
             ExpressionText.Text = Expression;
             CurrNumberText.Text = CurrNumberString;
+
+            if (Operation.OperationFlag == 1)
+            {
+                CurrNumberText.Text = "Cannot divide by zero";
+                CurrNumberString = "0";
+                Expression = "";
+                ExpressionText.Text = Expression;
+                LastEntry = '0';
+                Operation.OperationFlag = 0;
+            }
+            else if (Operation.OperationFlag == 2)
+            {
+                CurrNumberText.Text = "Result is undefined";
+                CurrNumberString = "0";
+                Expression = "";
+                ExpressionText.Text = Expression;
+                LastEntry = '0';
+                Operation.OperationFlag = 0;
+            }
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
+            Session["LastEntry"] = LastEntry;
+            Session["Operation"] = Operation;
         }
 
         protected void NumButton_Click(object sender, EventArgs e)
@@ -144,6 +236,11 @@ namespace Calculator
             LastEntry = Expression[Expression.Length - 1];
             
             CurrNumberText.Text = CurrNumberString;
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
+            Session["LastEntry"] = LastEntry;
+            Session["Operation"] = Operation;
         }
 
         protected void ButtonDOT_Click(object sender, EventArgs e)
@@ -166,6 +263,10 @@ namespace Calculator
             }
             
             CurrNumberText.Text = CurrNumberString;
+
+            Session["CurrNumberString"] = CurrNumberString;
+            Session["Expression"] = Expression;
+            Session["LastEntry"] = LastEntry;
         }
     }
 }
